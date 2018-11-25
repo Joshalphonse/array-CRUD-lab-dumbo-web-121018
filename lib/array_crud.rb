c72700b7232cea470861db127b7b835f1199a5d2
Joshua Alphonse <joshalphonse@gmail.com>
2018-11-25 19:40:51 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-dittatori = ["Hitler", "Mussolini", "Stalin", "Salvini"]
+pets = ["dog", "cat", "bear", "bird"]
 end
 
 def add_element_to_end_of_array(array, element)
